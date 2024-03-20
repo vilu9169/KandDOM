c2d6422dcd504bb88533f6e31631d24b6d0df000
vilu9169 <viktor.lundin3@gmail.com>
2024-03-20 11:33:00 +0100

    Start on SideMenu design

---
+++ frontend/src/components/sidemenu.js
@@ -0,0 +1,14 @@
+
+
+
+function SideMenu() {
+    return (
+    <div>
+        <div className=' h-100 position-absolute' style={{width:'40vh',backgroundColor:'grey'}}>
+
+        </div>
+    </div>
+    );
+}
+
+export default SideMenu;
