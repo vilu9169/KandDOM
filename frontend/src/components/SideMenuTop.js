5f12cd5682beabd0f7bcfec717493c385bd08b3c
pl33r <60385207+pl33r@users.noreply.github.com>
2024-03-26 13:02:05 +0100

    integrerade gitmerge via viktor

--- frontend/src/components/SideMenuTop.js
+++ frontend/src/components/SideMenuTop.js
@@ -1,13 +1,14 @@
 import { Button, Container } from "react-bootstrap";
-
+import { IoIosDocument } from "react-icons/io";
 
 
 function SideMenuTop() {
 
     return (
-        <Container className=" w-75 h-10 border-black border-bottom">
-            <Button className="top-menu-button">
-                Ladda upp dokument
+        <Container className="p-0 w-100 h-100 justify-content-center d-flex align-items-center">
+            <Button className="m-auto bg-3 w-90 wide-button d-flex justify-content-center align-items-center p-1">
+              <span className="text-center justify-content-center d-flex align-items-center w-75">Add document</span>
+              <span className="w-25 justify-content-center d-flex align-items-center"><IoIosDocument className="size-20" /></span>
             </Button>
         </Container>
     );
--- frontend/src/components/start.js
+++ frontend/src/components/start.js
@@ -3,10 +3,8 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import MyForm from "./Form";
-import UploadFileWindow from "./UploadFileWindow"
+import UploadFileWindow from "./UploadFileWindow";
 import { IoMdHelp } from "react-icons/io";
-
-
 
 import SideMenuBottom from "./SideMenuBottom";
 import SettingsMenu from "./SettingsMenu";
@@ -20,11 +18,12 @@ function Start() {
   return (
     <Container fluid className="position-absolute h-100 text-center bg-3">
       <Row className="h-100">
-        <Col color="" className="col-2 sidemenu">
-          <Row className="h-75 bg-2 position-relative">
+        <Col color="" className="col-2 sidemenu d-flex flex-column">
+          <Row className="h-60px bg-2 position-relative">
             <SideMenuTop />
           </Row>
-          <Row className="h-25  bg-2">
+          <Row className="flex-grow-1  bg-2">las</Row>
+          <Row className="h-80px  bg-2">
             <SideMenuBottom />
           </Row>
         </Col>
@@ -33,19 +32,17 @@ function Start() {
             <Row className="h-100 bg-1 m-auto">
               <Col className="col-6 h-100 bg-1 align-items-end d-flex justify-content-start">
                 <h2 className="p-0 m-0">Pythia</h2>
-                {/* Add company icon */ }
+                {/* Add company icon */}
               </Col>
               <Col className="col-6 h-100 d-flex align-items-end justify-content-end">
                 <Button className="bg-3 rect-button align-items-center d-flex justify-content-center p-0">
-                <IoMdHelp className="size-20" />
-
+                  <IoMdHelp className="size-20" />
                 </Button>
               </Col>
             </Row>
           </Row>
           <Row className="flex-grow-1 bg-1">
             <UploadFileWindow></UploadFileWindow>
-          
           </Row>
           <Row className="h-80px bg-1 align-items-top">
             <MyForm></MyForm>
