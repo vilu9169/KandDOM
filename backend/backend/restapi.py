986885147243bba0929e62bfc0727b4eb1c37495
Rikard Ahlkvist <68117552+Rickrokyfy@users.noreply.github.com>
2024-03-26 09:00:09 +0100

    Update restapi.py
    New keys

--- backend/backend/restapi.py
+++ backend/backend/restapi.py
@@ -34,8 +34,8 @@ def start_chat(project_id: str, location: str) -> str:
     # Set the endpoint URL
     #endpoint = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:streamGenerateContent "
     #Doesnt work bc no support for simple key but right otherwise
-    endpoint = f"https://us-central1-aiplatform.googleapis.com/v1/projects/sunlit-inn-417922/locations/us-central1/publishers/google/models/gemini-1.0-pro:generateContent"
-    #endpoint = f"https://us-central1-aiplatform.googleapis.com/v1/projects/sunlit-inn-417922/locations/us-central1/publishers/google/models/chat-bison:predict"
+    #endpoint = f"https://us-central1-aiplatform.googleapis.com/v1/projects/sunlit-inn-417922/locations/us-central1/publishers/google/models/gemini-1.0-pro:generateContent"
+    endpoint = f"https://us-central1-aiplatform.googleapis.com/v1/projects/sunlit-inn-417922/locations/us-central1/publishers/google/models/chat-bison:predict"
     
 
     # Set the request payload
@@ -63,18 +63,18 @@ def start_chat(project_id: str, location: str) -> str:
         "topK": 40
     }
     }
-    payload ={
-    "contents": {
-        "role": "user",
-        "parts": [
-        {
-            #Preprompt the model with example questions and answers
+    # payload ={
+    # "contents": {
+    #     "role": "user",
+    #     "parts": [
+    #     {
+    #         #Preprompt the model with example questions and answers
             
-            "text": "What is the capital of France?"
-        }
-        ]
-    }
-    }
+    #         "text": "What is the capital of France?"
+    #     }
+    #     ]
+    # }
+    # }
 
     # Set the request headers
     headers = {
@@ -84,8 +84,8 @@ def start_chat(project_id: str, location: str) -> str:
         #gcloud auth application-default print-access-token
         
         
-        #"Authorization": "Bearer ya29.a0Ad52N3_GY5n37jM0UdgsghJ8aku0aGoBpFyEde9193mIHIg89EqW0ZCACAditspsAPLdZmQDaAlyhlF58TXktmNwPWHp0ZJt1YjHlgV0kAV-_2h3POPmPPUU4F0s5oGP77xbh60QYUoq8SB9IEyzlAJ_XMNSjFOdCbVrxQX1bW4aCgYKAVQSARESFQHGX2MiXSlJdUidBCKwsGLY1p5Z2Q0178"
-        "Authorization": "Bearer ya29.a0Ad52N3-7qVPZa12Wse6BSJNQ24KZfkia7Gq4MlpP5wOBLNUKgOotIhW3ef3Rodk3qhQ6c2ytUjCrdj_eTyJ0GJDCoi_mzTEj4eTQSWsbid-nioSCgyM_VQXfrcb09Ejbhb6m8YIq3gYWYb8AZmivV7su6gij-F11yE2jaCgYKAXoSARESFQHGX2MiHTZTJzBemaPuTzEIvZbjSw0171"
+        "Authorization": "Bearer ya29.a0Ad52N3-YgV_BgDWyvKEaS9u_JBoPqWwhC5XxMVpuVeuBOuLNtVEUrF296xDEXh_L2XwINCZOFNtD2ca8fAdPuQFSb_lT4Fs9OPXB0zssxHATY8_J8dTeL8aMiprqNZFpPW2802oDs2myKMCfw869d7xobSDmcPP4E2rnJT4QWTgaCgYKARUSARESFQHGX2MimtuLGU7zZHmZRuMvlHOUuQ0178"
+        #"Authorization": "Bearer ya29.a0Ad52N3-TGy3ID4ifKiRrz7uGUGInWkfZJdyfSxHnU59HwaHvGC7o_7kdpPeUtRyCfAh1Zt31XmBL-0f7QVGD2gBmkQDur99ttU2Jvf3hRGYlJM91jkuTSaaKYuI9SLw8h6rswamNUsrbajbRtUu5CvfVXfKyKRQynJrDIwaCgYKAVQSARESFQHGX2MiVsLQnYvjtCqR-YP80egHww0173"
     }
     print("Requesting to start the chat...")
     # Send the POST request
