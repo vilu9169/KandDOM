a5a39639b285baa320b6583e9864e0e6ae5bcd29
vilu9169 <viktor.lundin3@gmail.com>
2024-03-27 17:12:28 +0100

    Fix chat to web attempt 44

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -50,8 +50,7 @@ def chat_view(request):
     #Create a json struct for previous messages and the current message
     odd = True
     messages = []
-    previous_messages = json.loads(messages_json) if messages_json else []
-    previous_messages = [msg['text'] for msg in previous_messages]
+    previous_messages = [msg['text'] for msg in messages_json]
     print('Received messages: ', previous_messages)  # Get all elements except the last one
     
     for message in previous_messages:
