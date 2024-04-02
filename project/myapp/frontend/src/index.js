9a377913ed0a14c00e747febff5c29462f31c4dd
vilu9169 <viktor.lundin3@gmail.com>
2024-04-02 18:25:05 +0200

    Login system attempt 7

--- project/myapp/frontend/src/index.js
+++ project/myapp/frontend/src/index.js
@@ -9,6 +9,7 @@ import 'react-perfect-scrollbar/dist/css/styles.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link rel="manifest" href="public/manifest.json" />
     <App />
   </React.StrictMode>
 );
