d0fb39e082e277eaa3fb5d6bb2919774f656e151
vilu9169 <viktor.lundin3@gmail.com>
2024-04-02 16:16:42 +0200

    Login system attempt 1

--- project/myapp/frontend/src/components/start.js
+++ project/myapp/frontend/src/components/start.js
@@ -11,59 +11,62 @@ import { IoMdHelp } from "react-icons/io";
 import SideMenuBottom from "./SideMenuBottom";
 import SettingsMenu from "./SettingsMenu";
 import { showInfoWindowContext } from "./ShowInfoWindowContextProvider.js";
-import { useContext, useState, createContext } from "react";
+import { useContext } from "react";
 import { AppContext } from "./ShowSettingsHandler";
 import SideMenuTop from "./SideMenuTop";
-import SideMenuFiles from "./SideMenuFiles.js";
 import Chatbot from "./Chatbot.js";
 import InfoWindow from "./InfoWindow.js";
-
+import { AuthContext } from "./AuthContextProvider";
 
 function Start() {
-
   const { buttonClicked } = useContext(AppContext);
-  const { showInfoWindow , handleShowInfo } = useContext(showInfoWindowContext);
-
+  const { showInfoWindow, handleShowInfo } = useContext(showInfoWindowContext);
+  const { user } = useContext(AuthContext);
   return (
-    <Container fluid className="position-absolute h-100 text-center bg-3">
-      <Row className="h-100">
-
-        <Col color="" className="col-2 sidemenu d-flex flex-column">
-          <Row className="h-60px bg-2 position-relative">
-            <SideMenuTop />
+    <>
+      {user && (
+        <Container fluid className="position-absolute h-100 text-center bg-3">
+          <Row className="h-100">
+            <Col color="" className="col-2 sidemenu d-flex flex-column">
+              <Row className="h-60px bg-2 position-relative">
+                <SideMenuTop />
+              </Row>
+              <Row className="flex-grow-1  bg-2">las</Row>
+              <Row className="h-80px  bg-2">
+                <SideMenuBottom />
+              </Row>
+            </Col>
+            <Col className="col-10 d-flex flex-column">
+              <Row className="h-60px bg-3">
+                <Row className="h-100 bg-1 m-auto">
+                  <Col className="col-6 h-100 bg-1 align-items-end d-flex justify-content-start">
+                    <h2 className="p-0 m-0">Pythia</h2>
+                    {/* Add company icon */}
+                  </Col>
+                  <Col className=" col-6 h-100 d-flex align-items-end justify-content-end">
+                    <Button
+                      onClick={handleShowInfo}
+                      className="bg-3 rect-button align-items-center d-flex justify-content-center p-0"
+                    >
+                      <IoMdHelp className="size-20" />
+                    </Button>
+                  </Col>
+                </Row>
+              </Row>
+              <Row className=" flex-grow-1 bg-1 position-relative">
+                {false && <UploadFileWindow></UploadFileWindow>}
+                <Chatbot></Chatbot>
+              </Row>
+              <Row className="h-80px bg-1 align-items-top">
+                <MyForm></MyForm>
+              </Row>
+            </Col>
           </Row>
-          <Row className="flex-grow-1  bg-2">las</Row>
-          <Row className="h-80px  bg-2">
-            <SideMenuBottom />
-          </Row>
-        </Col>
-        <Col className="col-10 d-flex flex-column">
-          <Row className="h-60px bg-3">
-            <Row className="h-100 bg-1 m-auto">
-              <Col className="col-6 h-100 bg-1 align-items-end d-flex justify-content-start">
-                <h2 className="p-0 m-0">Pythia</h2>
-                {/* Add company icon */}
-              </Col>
-              <Col className=" col-6 h-100 d-flex align-items-end justify-content-end">
-                <Button onClick={handleShowInfo} className="bg-3 rect-button align-items-center d-flex justify-content-center p-0">
-                  <IoMdHelp className="size-20" />
-                </Button>
-              </Col>
-            </Row>
-          </Row>
-
-          <Row className=" flex-grow-1 bg-1 position-relative">
-            {false && <UploadFileWindow></UploadFileWindow>}
-            <Chatbot></Chatbot>
-          </Row>
-          <Row className="h-80px bg-1 align-items-top">
-            <MyForm></MyForm>
-          </Row>
-        </Col>
-      </Row>
+        </Container>
+      )}
       {buttonClicked && <SettingsMenu />}
       {showInfoWindow && <InfoWindow />}
-    </Container>
+    </>
   );
 }
 
--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -1,15 +1,13 @@
 
 from django.urls import path
 from . views import *
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .views import MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('chat/', chat_view, name='chat_view'),
     path('', index, name='index'),
     path('test/', display_text_file, name='display_text'),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
