ae3aaa880a9557f7030c6b0c2dd6d2c4dffb1754
vilu9169 <viktor.lundin3@gmail.com>
2024-04-02 16:16:42 +0200

    Login system attempt 1

--- project/myapp/frontend/src/components/LogInLogic.js
+++ project/myapp/frontend/src/components/LogInLogic.js
@@ -1,23 +1,29 @@
 import { Button, Col, Container, FormGroup, Row, Form } from "react-bootstrap";
+import { useContext } from "react";
+import { AuthContext } from "./AuthContextProvider";
 
 function LogInLogic() {
+  let {loginUser} = useContext(AuthContext)
+
   return (
-    <Form className="m-auto">
+    <Form className="m-auto" onSubmit={ loginUser }>
       <FormGroup className="m-auto text-start login-group w-50">
         <Form.Control
           className="my-5 login-form"
+          name="username"
           autoComplete={false}
-          type="email"
-          placeholder="Enter email"
+          type="username"
+          placeholder="Enter username"
         />
         <Form.Control
+          name="password"
           className=" mt-5 login-form"
           type="password"
           placeholder="Password"
         />
         <Form.Text className="ms-3">Glömt lösenord?</Form.Text>
       </FormGroup>
-      <Button className="bg-4 border-0 mt-3" href="/">
+      <Button type="submit" className="bg-4 border-0 mt-3" href="/">
         Login
       </Button>
     </Form>
