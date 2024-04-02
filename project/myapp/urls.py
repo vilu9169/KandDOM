57ce50ea22aaa9c30303c7442067db40efef655f
vilu9169 <viktor.lundin3@gmail.com>
2024-04-03 01:11:55 +0200

    Säkerligen funkar inloggen nu :)

--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('test/', display_text_file, name='display_text'),
     path('token/', jwt_views.TokenObtainPairView.as_view(), name ="token_obtain_pair"),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
-    path('register/', RegisterView, name='register'),
 ]
