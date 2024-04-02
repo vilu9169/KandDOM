396e0688803d12b367421cccb78507b3d7923943
vilu9169 <viktor.lundin3@gmail.com>
2024-04-02 18:34:07 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- project/myapp/frontend/src/index.js
+++ project/myapp/frontend/src/index.js
@@ -9,7 +9,6 @@ import 'react-perfect-scrollbar/dist/css/styles.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <link rel="manifest" href="manifest.json" />
     <App />
   </React.StrictMode>
 );
