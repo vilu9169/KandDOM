3d0e84a1269bb9583f2188f8f23be1864db7bb77
pl33r <60385207+pl33r@users.noreply.github.com>
2024-04-02 18:49:09 +0200

    Login/signup drip upgrade

--- project/myapp/frontend/src/components/SignUpLogic.js
+++ project/myapp/frontend/src/components/SignUpLogic.js
@@ -1,11 +1,11 @@
 import { Button, Col, Container, FormGroup, Row, Form } from "react-bootstrap";
 import { IoIosLogIn } from "react-icons/io";
 
-function LogInLogic({ onSignUpClick }) {
+function SignUpLogic({ onLoginClick }) {
   return (
     <Form className="m-auto h-100">
       <Row className="h-25 bg-1 m-auto w-100 d-flex justify-content-center align-items-center">
-        <h2 className="p-0 m-0">Welcome Back!</h2>
+        <h2 className="p-0 m-0">Create an account</h2>
       </Row>
       <Row className="h-25 bg-1 m-auto w-100 d-flex justify-content-center align-items-center">
         <Form.Control
@@ -24,21 +24,15 @@ function LogInLogic({ onSignUpClick }) {
         {/* <Form.Text className="ms-3 w-25">Glömt lösenord?</Form.Text>*/}
       </Row>
       <Row className="h-25 bg-1 m-auto w-100 d-flex justify-content-center align-items-center">
-        <Button
-          className="m-auto bg-3 w-90 wide-button d-flex justify-content-center align-items-center p-1"
-          href="/"
-        >
-          <span className="text-center justify-content-center d-flex align-items-center w-75">
-            Log in
-          </span>
-          <span className="w-25 justify-content-center d-flex align-items-center">
-            <IoIosLogIn className="size-20" />
+        <Button className="m-auto bg-3 w-90 wide-button d-flex justify-content-center align-items-center p-1">
+          <span className="text-center justify-content-center d-flex align-items-center w-100">
+            Sign up
           </span>
         </Button>
         <Form.Text className="w-90">
-          Don't have an account?{" "}
-          <span onClick={onSignUpClick} className="click-text">
-            Sign Up
+          Already have an account?{" "}
+          <span onClick={onLoginClick} className="click-text">
+            Login
           </span>
         </Form.Text>
       </Row>
@@ -46,4 +40,4 @@ function LogInLogic({ onSignUpClick }) {
   );
 }
 
-export default LogInLogic;
+export default SignUpLogic;
