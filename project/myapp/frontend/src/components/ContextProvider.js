c2edf9fc3ced463207cada68221efc7bef336c6f
vilu9169 <viktor.lundin3@gmail.com>
2024-04-03 23:38:10 +0200

    Signup added

--- project/myapp/frontend/src/components/ContextProvider.js
+++ project/myapp/frontend/src/components/ContextProvider.js
@@ -4,20 +4,23 @@ import { AppProvider } from "./ShowSettingsHandler";
 import { DarkModeContextProvider } from "./DarkModeContextProvider";
 import { ShowInfoWindowContextProvider } from "./ShowInfoWindowContextProvider";
 import { AuthContextProvider } from "./AuthContextProvider";
+import { UploadWindowContextProvider } from "./UploadWindowContextProvider";
 
 function ContextProvider({ children }) {
   return (
-    <AuthContextProvider>
-      <ShowInfoWindowContextProvider>
-        <DarkModeContextProvider>
-          <ResponseContextProvider>
-            <MessageContextProvider>
-              <AppProvider>{children}</AppProvider>
-            </MessageContextProvider>
-          </ResponseContextProvider>
-        </DarkModeContextProvider>
-      </ShowInfoWindowContextProvider>
-    </AuthContextProvider>
+    <UploadWindowContextProvider>
+      <AuthContextProvider>
+        <ShowInfoWindowContextProvider>
+          <DarkModeContextProvider>
+            <ResponseContextProvider>
+              <MessageContextProvider>
+                <AppProvider>{children}</AppProvider>
+              </MessageContextProvider>
+            </ResponseContextProvider>
+          </DarkModeContextProvider>
+        </ShowInfoWindowContextProvider>
+      </AuthContextProvider>
+    </UploadWindowContextProvider>
   );
 }
 
