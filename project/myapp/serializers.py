d177111ca8f4944ead90a869402250eb3cb33999
vilu9169 <viktor.lundin3@gmail.com>
2024-04-04 12:47:55 +0200

    Error display login

--- project/myapp/serializers.py
+++ project/myapp/serializers.py
@@ -8,14 +8,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'email', 'password']
         
     def create(self, validated_data):
-        user_exist = User.objects.filter(email=validated_data.get("email")).exists()
-        if user_exist:
             password = validated_data.pop("password")
             instance = self.Meta.model(**validated_data)
-            email = validated_data.get("email")
             if password is not None:
                 instance.set_password(password)
             instance.save()
             return instance
-        else:
-            return serializers.ValidationError("User already exists")
