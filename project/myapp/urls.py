3643633fcd1135fdb4a68399e633fd852ef3a940
vilu9169 <viktor.lundin3@gmail.com>
2024-04-08 11:33:55 +0200

    Small URL change

--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -1,13 +1,17 @@
 
 from django.urls import path, re_path
+from django.conf.urls import url
 from . views import *
 from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework_simplejwt import views as jwt_views
 from django.views.generic import TemplateView
+from django.conf import settings
 
+
+routes = getattr(settings, 'REACT_ROUTES', [])
 urlpatterns = [
     path('chat/', chat_view, name='chat_view'),
-    re_path(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^(%s)?$' % '|'.join(routes), TemplateView.as_view(template_name='index.html')),
     path('test/', display_text_file, name='display_text'),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name ="token_obtain_pair"),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
