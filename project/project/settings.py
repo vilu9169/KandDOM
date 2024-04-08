d97eca877cd5426b99ccdce85fc2113df60d0009
vilu9169 <viktor.lundin3@gmail.com>
2024-04-08 10:43:09 +0200

    Small URL change

--- project/project/settings.py
+++ project/project/settings.py
@@ -190,6 +190,7 @@ AWS_SECRET_ACCESS_KEY = 'SLDBCr/Ox+8tAzD0tEnb3GdyqApTN1fv54j2gebA'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myapp/frontend', 'build', 'static'),
 ]
+
 WEBPACK_LOADER = {
     'MANIFEST_FILE': os.path.join(BASE_DIR, "myapp/frontend/build/manifest.json"),
 }
