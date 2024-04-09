4385d3271c8db1f2782caddf85e49bc5d72d84a6
vilu9169 <viktor.lundin3@gmail.com>
2024-04-09 14:21:16 +0200

    Small change pass data upload

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -301,7 +301,7 @@ def upload_document(request):
 
         document.save()
         user = User.objects.get(id=request.data['userID'])
-        user.documents.append(document.id)  # Add the document ID to the user's documents list
+        user.documents.append(document._id)  # Add the document ID to the user's documents list
         user.save() 
         # You might want to return the ID of the newly created document for future reference
         return Response({'document_id ': str(document._id)})
