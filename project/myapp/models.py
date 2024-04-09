6e19ecaea1292ff21813a7d4d2bd1dd84937a75b
vilu9169 <viktor.lundin3@gmail.com>
2024-04-09 16:01:47 +0200

    Small change pass data upload

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -9,7 +9,7 @@ from djongo import models as djmodels
 from djongo.models.fields import ArrayReferenceField
 
 class Document(models.Model):
-    id = models.BigAutoField( primary_key=True, editable=False, db_column='_id')
+    _id = djmodels.ObjectIdField()
     file=models.FileField(upload_to='pdf/')
     filename = models.CharField(max_length=255)
     content_type = models.CharField(max_length=100)
