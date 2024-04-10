7361739fe8ec4717f25d2aaa40c684ab935ffa9f
CarlJohan12 <90687192+CarlJohan12@users.noreply.github.com>
2024-04-10 11:56:19 +0200

    Update models.py

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -58,7 +58,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractUser):
     #_id = djmodels.ObjectIdField()
-    id = id = models.BigAutoField( primary_key=True, editable=False, db_column='id')
+    id = models.BigAutoField( primary_key=True, editable=False, db_column='id')
     name = models.CharField(max_length=250)
     email = models.CharField(max_length=250, unique=True)
     password = models.CharField(max_length=250)
