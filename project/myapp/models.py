420176fd7b6830a1079b15e6eec4fe421360f68f
CarlJohan12 <90687192+CarlJohan12@users.noreply.github.com>
2024-04-10 11:31:52 +0200

    Trying to fix userid

--- project/myapp/models.py
+++ project/myapp/models.py
@@ -10,7 +10,7 @@ from djongo.models.fields import ArrayReferenceField
 
 class Document(models.Model):
     #_id = djmodels.ObjectIdField()
-    #id = models.BigAutoField( primary_key=True, editable=False, db_column='_id')
+    id = models.BigAutoField( primary_key=True, editable=False, db_column='_id')
     file=models.FileField(upload_to='pdf/')
     filename = models.CharField(max_length=255)
     content_type = models.CharField(max_length=100)
