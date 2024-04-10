e50ad2b8d6de92e18202d91728a9f3b08f258b0c
vilu9169 <viktor.lundin3@gmail.com>
2024-04-10 14:56:55 +0200

    Small change pass data upload

--- project/myapp/frontend/src/components/SideMenuFiles.js
+++ project/myapp/frontend/src/components/SideMenuFiles.js
@@ -14,24 +14,24 @@ function SideMenuFiles() {
   const body = {
     "userID": userID,
   }
-  const getFiles = async (e) => {
-    try {
-      const response = await axios.post("http://ec2-16-171-79-116.eu-north-1.compute.amazonaws.com:8000/api/documents/", body);
-      const data = await response.json();
-      console.log(data);
-      let fileArr = []
-      for (const file in data) {
-        console.log(file);
-        fileArr.push(file.name);
-      }
-      setFiles(fileArr);
-      return data;
-    }
-    catch (error) {
-      console.error("Error fetching files:", error);
-    }
-  };
-  getFiles()
+  // const getFiles = async (e) => {
+  //   try {
+  //     const response = await axios.post("http://ec2-16-171-79-116.eu-north-1.compute.amazonaws.com:8000/api/documents/", body);
+  //     const data = await response.json();
+  //     console.log(data);
+  //     let fileArr = []
+  //     for (const file in data) {
+  //       console.log(file);
+  //       fileArr.push(file.name);
+  //     }
+  //     setFiles(fileArr);
+  //     return data;
+  //   }
+  //   catch (error) {
+  //     console.error("Error fetching files:", error);
+  //   }
+  // };
+  // getFiles()
   return (
     <Container className="h-100 file-window">
       <h3 className=" mt-3 ms-3 text-start">Filer</h3>
