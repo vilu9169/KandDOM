e15c0bb22bd5387bef1449ccacd15811b4b9ab7b
Björn Westerlund <bpf.westerlund@gmail.com>
2024-04-11 15:32:36 +0200

    document stuff

---
+++ backend/tointegrate/langgraph_tests/conditional.py
@@ -0,0 +1,166 @@
+from langgraph.prebuilt import ToolExecutor
+from langchain_google_vertexai import VertexAI
+from langchain.tools import BaseTool, StructuredTool, tool
+from langgraph.graph import END, MessageGraph
+
+
+
+import json
+from langchain_core.messages import ToolMessage
+from langchain_core.tools import tool
+from langchain_core.utils.function_calling import convert_to_openai_tool
+
+@tool
+def multiply(first_number: int, second_number: int):
+    """Multiplies two numbers together."""
+    return first_number * second_number
+
+model = VertexAI(model_name="gemini-pro")
+
+model_with_tools = model.bind(tools=[convert_to_openai_tool(multiply)])
+
+graph = MessageGraph()
+
+def invoke_model(state: List[BaseMessage]):
+    return model_with_tools.invoke(state)
+
+graph.add_node("oracle", invoke_model)
+
+def invoke_tool(state: List[BaseMessage]):
+    tool_calls = state[-1].additional_kwargs.get("tool_calls", [])
+    multiply_call = None
+
+    for tool_call in tool_calls:
+        if tool_call.get("function").get("name") == "multiply":
+            multiply_call = tool_call
+
+    if multiply_call is None:
+        raise Exception("No adder input found.")
+
+    res = multiply.invoke(
+        json.loads(multiply_call.get("function").get("arguments"))
+    )
+
+    return ToolMessage(
+        tool_call_id=multiply_call.get("id"),
+        content=res
+    )
+
+graph.add_node("multiply", invoke_tool)
+
+graph.add_edge("multiply", END)
+
+graph.set_entry_point("oracle")
+
+
+tools = [multiply]
+
+tool_executor = ToolExecutor(tools)
+
+
+from langgraph.prebuilt import ToolInvocation
+import json
+from langchain_core.messages import FunctionMessage
+
+# Define the function that determines whether to continue or not
+def should_continue(state):
+    messages = state['messages']
+    last_message = messages[-1]
+    # If there is no function call, then we finish
+    if "function_call" not in last_message.additional_kwargs:
+        return "end"
+    # Otherwise if there is, we continue
+    else:
+        return "continue"
+
+# Define the function that calls the model
+def call_model(state):
+    messages = state['messages']
+    response = model.invoke(messages)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [response]}
+
+# Define the function to execute tools
+def call_tool(state):
+    messages = state['messages']
+    # Based on the continue condition
+    # we know the last message involves a function call
+    last_message = messages[-1]
+    # We construct an ToolInvocation from the function_call
+    action = ToolInvocation(
+        tool=last_message.additional_kwargs["function_call"]["name"],
+        tool_input=json.loads(last_message.additional_kwargs["function_call"]["arguments"]),
+    )
+    # We call the tool_executor and get back a response
+    response = tool_executor.invoke(action)
+    # We use the response to create a FunctionMessage
+    function_message = FunctionMessage(content=str(response), name=action.tool)
+    # We return a list, because this will get added to the existing list
+    return {"messages": [function_message]}
+
+
+from typing import TypedDict, Annotated, Sequence
+import operator
+from langchain_core.messages import BaseMessage
+
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+
+
+
+from langgraph.graph import StateGraph, END
+# Define a new graph
+workflow = StateGraph(AgentState)
+
+# Define the two nodes we will cycle between
+workflow.add_node("agent", call_model)
+workflow.add_node("action", call_tool)
+
+# Set the entrypoint as `agent`
+# This means that this node is the first one called
+workflow.set_entry_point("agent")
+
+# We now add a conditional edge
+workflow.add_conditional_edges(
+    # First, we define the start node. We use `agent`.
+    # This means these are the edges taken after the `agent` node is called.
+    "agent",
+    # Next, we pass in the function that will determine which node is called next.
+    should_continue,
+    # Finally we pass in a mapping.
+    # The keys are strings, and the values are other nodes.
+    # END is a special node marking that the graph should finish.
+    # What will happen is we will call `should_continue`, and then the output of that
+    # will be matched against the keys in this mapping.
+    # Based on which one it matches, that node will then be called.
+    {
+        # If `tools`, then we call the tool node.
+        "continue": "action",
+        # Otherwise we finish.
+        "end": END
+    }
+)
+
+# We now add a normal edge from `tools` to `agent`.
+# This means that after `tools` is called, `agent` node is called next.
+workflow.add_edge('action', 'agent')
+
+# Finally, we compile it!
+# This compiles it into a LangChain Runnable,
+# meaning you can use it as you would any other runnable
+app = workflow.compile()
+
+print("graph compiled")
+
+from langchain_core.messages import HumanMessage
+
+inputs = {"messages": [HumanMessage("What is 3 * 5?")]}
+
+for output in app.stream(inputs):
+    # stream() yields dictionaries with output keyed by node name
+    for key, value in output.items():
+        print(f"Output from node '{key}':")
+        print("---")
+        print(value)
+    print("\n---\n")
