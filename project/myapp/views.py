db6dbd21c1e8cc0d1e0fdd5294e801c26101a66a
vilu9169 <viktor.lundin3@gmail.com>
2024-04-15 10:51:37 +0200

    new chat thing

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -127,7 +127,7 @@ def chat_view(request):
 import subprocess
 import requests
 
-def start_chat(input, previous_messages) -> str:
+def start_chat2(input, previous_messages) -> str:
     endpoint = f"https://us-central1-aiplatform.googleapis.com/v1/projects/sunlit-inn-417922/locations/us-central1/publishers/google/models/chat-bison:predict"
     
     #Load document from output.txt
@@ -331,3 +331,136 @@ def get_documents(request):
     documents = user.documents.all()
     serializer = DocumentSerializer(documents, many=True)
     return Response(serializer.data)
+
+
+from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_google_vertexai import VertexAI
+
+
+from pinecone import Pinecone, ServerlessSpec, PodSpec  
+
+pc = Pinecone(api_key="2e669c83-1a4f-4f19-a06a-42aaf6ea7e06")
+index_name = "langchain-demo"
+index = pc.Index(index_name)  
+#print(index.describe_index_stats())
+
+embeddings = VertexAIEmbeddings(model_name="textembedding-gecko-multilingual@001")
+
+from langchain_pinecone import PineconeVectorStore  
+
+vectorstore = PineconeVectorStore(  
+    index, embeddings  
+)  
+llm = VertexAI()
+
+import subprocess
+import requests
+
+def start_chat(input, previous_messages) -> str:
+    # Set the endpoint URL
+    endpoint = f"https://us-central1-aiplatform.googleapis.com/v1/projects/sunlit-inn-417922/locations/us-central1/publishers/google/models/chat-bison:predict"
+    #endpoint = f"https://us-central1-aiplatform.googleapis.com/v1/projects/sunlit-inn-417922/locations/us-central1/publishers/google/models/gemini-1.5-pro:predict"
+    
+    
+    
+    context = "Du analyserar juridiska dokument för att underlätta arbete med dem. Du ska svara sakligt, opartiskt och enbart använda information från detta dokument i dina svar.  Detta är de RAG delar av dokument du har att tillgå :" 
+    index = 0
+    prepend = ""
+    append = ""
+    for rag in vectorstore.as_retriever(search_type="mmr", search_kwargs = ({"k" : 40, })).invoke(input):
+        #The first 10 documents are prepended to the context
+        #The last 10 documents are appended to append
+        if index < 10:
+            prepend += rag.page_content
+        elif index >10 and index < 20:
+            append = rag.page_content + append
+        else:
+            prepend += rag.page_content
+        index += 1
+        #Extract text from document
+    context += prepend + append
+    #print("Context: ", context)
+    #Create a json struct for previous messages and the current message
+    messages = []
+    odd = True
+    for message in previous_messages:
+        if odd:
+            messages.append({
+                "author": "user",
+                "content": message
+            })
+            odd = False
+        else:
+            messages.append({
+                "author": "model",
+                "content": message
+            })
+            odd = True
+    messages.append({
+        "author": "user",
+        "content": input
+    })
+    
+    payload = {
+    "instances": [{
+        "context":  context,
+         "examples": [ 
+         {
+             "input": {"content": "Är den åtalade skyldig?"},
+             "output": {"content": "Jag är en opartisk assistent och är inte kapabel att besvara denna fråga. "}
+         }],
+        "messages": messages,
+    }],
+    "parameters": {
+        "temperature": 0.3,
+        "maxOutputTokens": 2000,
+        "topP": 0.8,
+        "topK": 40
+    }
+    }
+    auth = subprocess.check_output("gcloud auth print-access-token", shell=True)
+    
+    #Convert auth to string and remove last \r\n if on windows
+    if(auth[-2] == 13):
+        auth = auth.decode("utf-8")[:-2]
+    else:
+        auth = auth.decode("utf-8")[:-1]
+    # Set the request headers
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer "+auth
+           }
+    # Send the POST request
+    response = requests.post(endpoint, json=payload, headers=headers)
+
+    # Check the response status code
+    if response.status_code == 200:
+        #Get the response
+        resp = response.text
+        #print("Response: ", resp)
+        #Response is a json object so convert it to a json object
+        import json
+        resp = json.loads(resp)
+        #Get the response
+        resp = resp["predictions"][0]["candidates"][0]["content"]
+        return  Response({"message" : resp})
+    else:
+        print(response.text)
+        print(response.status_code)
+        return "Failed to start the chat"
+
+
+
+# Call the function with your project ID and location
+# prevmessages = []
+# while(True):
+#     #Prompt user for input
+#     print("Enter your message: ")
+#     message = input()
+#     res = start_chat(message, prevmessages)
+#     prevmessages.append(message)
+#     prevmessages.append(res)
+#     print(res)
+
+
+
