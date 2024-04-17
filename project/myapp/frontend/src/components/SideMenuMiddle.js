2f2bdaa0763c4d32ec3894c480613f56c2511c0d
vilu9169 <viktor.lundin3@gmail.com>
2024-04-17 14:44:18 +0200

    prod

--- project/myapp/frontend/src/components/SideMenuMiddle.js
+++ project/myapp/frontend/src/components/SideMenuMiddle.js
@@ -16,9 +16,9 @@ function SideMenuMiddle() {
   const handleButtonClick = () => {
     setShowTimeline(!showTimeline);
   };
-  const chooseDocument = (file) => {
-    console.log('file:', file.id)
-    setCurrentFile(file.id);
+  const chooseDocument = (fileid) => {
+    console.log('file:', fileid)
+    setCurrentFile(fileid);
     setMessages([]);
   };
 
@@ -39,7 +39,7 @@ function SideMenuMiddle() {
       <PerfectScrollbar>
       {files.map((file) => (
         <Row className=" my-4 overflow-scroll m-auto rounded-2 w-100 bg-3">
-          <Button value={file} onClick={e => chooseDocument(e.target.value)} className="my-2 text-start"><IoIosDocument  size={30} /> { file.filename } </Button>
+          <Button value={file.id} onClick={e => chooseDocument(e.target.value)} className="my-2 text-start"><IoIosDocument  size={30} /> { file.filename } </Button>
         </Row>
         ), [files])}
         </PerfectScrollbar>
