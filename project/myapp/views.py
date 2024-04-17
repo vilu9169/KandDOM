29e6b3781d92b5dbb654ddef4d34f56d2b6f63d1
vilu9169 <viktor.lundin3@gmail.com>
2024-04-17 13:18:37 +0200

    prod

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -374,7 +374,7 @@ import requests
 @api_view(['POST'])
 def start_chat(request):
     print("Starting chat")
-    print(request.data).get('index_name')
+    print(request.data.get('index_name'))
     index_name = request.data.get('index_name')
     index = pc.Index(index_name)
     vectorstore = PineconeVectorStore(  
