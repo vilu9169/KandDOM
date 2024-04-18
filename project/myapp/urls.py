53e74837cf908c93b90e863258f2d7f46506b4b2
jabbe987 <jakobosterlund98@gmail.com>
2024-04-18 16:49:52 +0200

    Reapply "Merge branch 'main' into jakob"
    This reverts commit fd1ec6f09d5c689851760cd85c62ea8df936378c.

--- project/myapp/urls.py
+++ project/myapp/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name ="token_obtain_pair"),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
     path('api/signup/', RegisterView.as_view(), name='register'),
+    path('api/documents/', get_documents, name='documents'),
 ]
