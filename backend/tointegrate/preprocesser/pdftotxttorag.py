a330a563ebc802d36a9d81ddd3392d2288280e74
Rickrokyfy <01ahlkvist@gmail.com>
2024-04-19 13:15:03 +0200

    Made it split documents correctly

--- backend/tointegrate/preprocesser/pdftotxttorag.py
+++ backend/tointegrate/preprocesser/pdftotxttorag.py
@@ -7,11 +7,12 @@ from langchain_google_vertexai import VertexAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from langchain.schema.document import Document
 from pinecone import Pinecone, ServerlessSpec
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+#from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
+
 from google.cloud import documentai
 import io
 import time
-
 def ocr_pdf(pdf_file, project_id, location, processor_id):
     
     # You must set the api_endpoint if you use a location other than 'us'.
@@ -24,41 +25,40 @@ def ocr_pdf(pdf_file, project_id, location, processor_id):
     # You must create new processors in the Cloud Console first
     name = client.processor_path(project_id, location, processor_id)
     # Create a working directory
-    working_dir = "./working_directory"
-    os.makedirs(working_dir, exist_ok=True)
 
     # Split the document into chunks of 20 pages
     chunk_size = 15
     reader = PyPDF2.PdfReader(pdf_file)
     num_pages = len(reader.pages)
-    num_chunks = num_pages//chunk_size + 1
+    if(num_pages%chunk_size == 0):
+        num_chunks = num_pages//chunk_size
+    else:
+        num_chunks = num_pages//chunk_size + 1
     resstring = ""
     pagenr = 0
     for chunk_num in range(num_chunks):
         start_page = chunk_num * chunk_size
         end_page = min((chunk_num + 1) * chunk_size, num_pages)
+        print("Working with pages ", start_page, " to ", end_page)
         #Previously created new file for each chunk
-        chunk_file = os.path.join(working_dir, f"writteto.pdf")
+        #chunk_file = os.path.join(working_dir, f"writteto.pdf")
         #Calculate time it takes to generate the chunk
-        starttime = time.time()
         # Extract the pages and save them as a new PDF file
         writer = PyPDF2.PdfWriter()
         for page_num in range(start_page, end_page):
             writer.add_page(reader.pages[page_num])
-            
-        with open(chunk_file, "wb") as f:
-            writer.write(f)
-        
+        response_bytes_stream = io.BytesIO()
+        writer.write(response_bytes_stream)
+        #Seek 0 to start reading
+        response_bytes_stream.seek(0)
         # Read the file into memory
-        with io.open(chunk_file, "rb") as image:
+        with response_bytes_stream as image:
             image_content = image.read()
-        print("Time to generate chunk: ", time.time()-starttime)
         # Load Binary Data into Document AI RawDocument Object
         raw_document = documentai.RawDocument(content=image_content, mime_type="application/pdf")
 
         # Configure the process request
         request = documentai.ProcessRequest(name=name, raw_document=raw_document)
-        print("Sending request to the processor service")
         result = client.process_document(request=request, )
         #print("Result looks like ", result)
 
@@ -72,11 +72,13 @@ def ocr_pdf(pdf_file, project_id, location, processor_id):
                 # Block content: [start_index: 15448
                 # end_index: 15463
                 # ]
-                print(f"Block content: {block.layout.text_anchor.text_segments}")
                 split = str(block.layout.text_anchor.text_segments).split("\n")
-                print("splitlen", len(split))
                 if(len(split) > 2):
                     resstring += document.text[int(split[0][split[0].find(":") +2:]) : int(split[1][split[1].find(":") +2:])]
+                else:
+                    resstring += document.text[0: int(split[0][split[0].find(":") +2:])]
+                    
+                
                 # if not("start_index" in str(block.layout.text_anchor.text_segments)):
                 #     #Extract end_index using string slicing
                     
@@ -85,7 +87,7 @@ def ocr_pdf(pdf_file, project_id, location, processor_id):
                 #     
                 
             #resstring += page.layout.text_anchor.content
-            resstring +="{pageend nr "+ str(pagenr+1) +"}"
+            resstring +="{pageend nr "+ str(pagenr+1) +"}"+str(chr(28))
             pagenr += 1
     print("Resstring",resstring)
     return resstring
@@ -101,7 +103,7 @@ def extract_text_from_pdf(pdf_file) -> str:
                 text += "{pagestart nr "+ str(page_num+1) +"}"
                 page = reader.pages[page_num]
                 text += page.extract_text()
-                text +="{pageend nr "+ str(page_num+1) +"}"
+                text +="{pageend nr "+ str(page_num+1) +"}"+str(chr(28))
 
             return text
     except FileNotFoundError:
@@ -124,7 +126,7 @@ def text_to_rag(new_index_name, text):
     # Split documents
     #text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
     #splits = [Document(page_content=x) for x in text_splitter.split_text(text)]
-    text_splitter = RecursiveCharacterTextSplitter(separators=["{pagestart", "{pageend"], chunk_overlap = 150)
+    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap =50,separator=str(chr(28)))
     splits = [Document(page_content=x) for x in text_splitter.split_text(text)]
     #splits = text_splitter.split_text(text)    
     embeddings = VertexAIEmbeddings(model_name="textembedding-gecko-multilingual@001")
@@ -142,4 +144,4 @@ print("Name of the file to be converted to RAG: ")
 pdf_file = input()
 print("Name of the new index: ")
 new_index_name = input()
-mainfunk("../output.pdf", "googlesown2")
+mainfunk(pdf_file, new_index_name)
