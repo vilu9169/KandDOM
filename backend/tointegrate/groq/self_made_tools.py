4a9825d4a1b4b432d536f2be451f2097b30c0183
Björn Westerlund <bpf.westerlund@gmail.com>
2024-04-22 22:57:08 +0200

    groq collection stuff

--- backend/tointegrate/groq/self_made_tools.py
+++ backend/tointegrate/groq/self_made_tools.py
@@ -1,37 +1,53 @@
 tools = {
-    "NBA" : {
-            "type": "function",
-            "function": {
-                "name": "get_game_score",
-                "description": "Get the score for a given NBA game",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "team_name": {
-                            "type": "string",
-                            "description": "The name of the NBA team (e.g. 'Golden State Warriors')",
-                        }
+    "NBA": {
+        "type": "function",
+        "function": {
+            "name": "get_game_score",
+            "description": "Get the score for a given NBA game",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "team_name": {
+                        "type": "string",
+                        "description": "The name of the NBA team (e.g. 'Golden State Warriors')",
+                    }
+                },
+                "required": ["team_name"],
+            },
+        },
+    },
+    "weather": {
+        "type": "function",
+        "function": {
+            "name": "get_weather",
+            "description": "Get the weather for a given location",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "location": {
+                        "type": "string",
+                        "description": "The location for which to get the weather (e.g. 'San Francisco')",
+                    }
+                },
+                "required": ["location"],
+            },
+        },
+    },
+    "ny_person": {
+        "type": "function",
+        "function": {
+            "name": "lägg_till_person",
+            "description": "Använd för att registrera en tidigare inte känd person",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "namn": {"type": "string", "description": "Namnet på personen, både förnamn och efternamn om tillgängligt"},
+                    "information": {
+                        "type": "string",
+                        "description": "Sammanställ information om personen",
                     },
-                    "required": ["team_name"],
                 },
             },
         },
-
-    "weather" : {
-            "type": "function",
-            "function": {
-                "name": "get_weather",
-                "description": "Get the weather for a given location",
-                "parameters": {
-                    "type": "object",
-                    "properties": {
-                        "location": {
-                            "type": "string",
-                            "description": "The location for which to get the weather (e.g. 'San Francisco')",
-                        }
-                    },
-                    "required": ["location"],
-                },
-            },
-        },
+    },
 }
