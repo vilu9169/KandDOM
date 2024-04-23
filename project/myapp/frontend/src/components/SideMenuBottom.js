9a3726be386e59070df7142198d35e0271b93e80
CarlJohan12 <carljohan.eklund.2@gmail.com>
2024-04-23 14:35:22 +0200

    update frontend

--- project/myapp/frontend/src/components/SideMenuBottom.js
+++ project/myapp/frontend/src/components/SideMenuBottom.js
@@ -12,19 +12,29 @@ import { Transition } from "react-transition-group";
 import { AppContext } from "./ShowSettingsHandler";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 import LogIn from "./LogIn";
-
 import Collapse from "react-bootstrap/Collapse";
 import Fade from "react-bootstrap/Fade";
-
 import { AuthContext } from "./AuthContextProvider";
+import { TbTimelineEventFilled } from "react-icons/tb";
+import TimeLine from "./TimeLine";
 
-function SideMenuBottom() {
+function SideMenuBottom({ clickedDocument }) {
   const [isVisible, setIsVisible] = useState(false);
   const { handleButtonClick } = useContext(AppContext);
   const innerContainerRef = useRef(null);
   const personRef = useRef(null);
   const navigate = useNavigate();
   const { logoutUser } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
+
+  const [userName] = useState(user.name);
+
+  const [showTimeline, setShowTimeline] = useState(false);
+
+  const handleTimelineButtonClick = () => {
+    setShowTimeline(!showTimeline);
+  };
+
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
   };
@@ -51,55 +61,75 @@ function SideMenuBottom() {
   }, [toggleVisibility]); // Include handleButtonClick in the dependency array
 
   return (
-      <Container className="p-0 m-0 position-absolute bottom-0 start-50 translate-middle-x mb-3">
-        <Fade mountOnEnter={true} unmountOnExit={true} appear={isVisible} in={isVisible} >
-          <Container
-            className="p-0 mb-2  w-90 bg-1 user-pop-up-container"
-            ref={innerContainerRef}
-          >
-            <Row className="p-0 m-0 h-50 w-100 ">
-              <Button
-                onClick={handleButtonClick}
-                className="m-auto bg-3 w-90 pop-up-button d-flex justify-content-center align-items-center p-1"
-              >
-                <span className="text-center justify-content-center d-flex align-items-center w-75">
-                  Settings
-                </span>
-                <span className="w-25 justify-content-center d-flex align-items-center">
-                  <IoIosSettings className="size-20" />
-                </span>
-              </Button>
-            </Row>
-            <Row className="p-0 m-0 h-50 w-100">
-              {" "}
-              <Button
-                onClick={logoutUser}
-                className="m-auto bg-3 w-90 pop-up-button d-flex justify-content-center align-items-center p-1"
-              >
-                <span className="text-center justify-content-center d-flex align-items-center w-75">
-                  Log Out
-                </span>
-                <span className="w-25 justify-content-center d-flex align-items-center">
-                  <IoIosLogOut className="size-20" />
-                </span>
-              </Button>
-            </Row>
-          </Container>
-        </Fade>
+    <Container className="p-0 m-0 position-absolute bottom-0 start-50 translate-middle-x mb-3">
+      {clickedDocument && (
         <Button
-          ref={personRef}
-          onClick={toggleVisibility}
-          className="m-auto bg-3 w-90 wide-button d-flex justify-content-center align-items-center p-1"
+          onClick={handleTimelineButtonClick}
+          className="m-auto mb-3 bg-3 w-90 wide-button d-flex justify-content-center align-items-center p-1"
         >
           <span className="text-center justify-content-center d-flex align-items-center w-75">
-            Julius amorm
+            Timeline
           </span>
           <span className="w-25 justify-content-center d-flex align-items-center">
-            <IoMdPerson className="size-20" />
+            <TbTimelineEventFilled className="size-20" />
           </span>
         </Button>
-      </Container>
+      )}
 
+      <Fade
+        mountOnEnter={true}
+        unmountOnExit={true}
+        appear={isVisible}
+        in={isVisible}
+      >
+        <Container
+          className="p-0 mb-2  w-90 bg-1 user-pop-up-container"
+          ref={innerContainerRef}
+        >
+          <Row className="p-0 m-0 h-50 w-100 ">
+            <Button
+              onClick={handleButtonClick}
+              className="m-auto bg-3 w-90 pop-up-button d-flex justify-content-center align-items-center p-1"
+            >
+              <span className="text-center justify-content-center d-flex align-items-center w-75">
+                Settings
+              </span>
+              <span className="w-25 justify-content-center d-flex align-items-center">
+                <IoIosSettings className="size-20" />
+              </span>
+            </Button>
+          </Row>
+          <Row className="p-0 m-0 h-50 w-100">
+            {" "}
+            <Button
+              onClick={logoutUser}
+              className="m-auto bg-3 w-90 pop-up-button d-flex justify-content-center align-items-center p-1"
+            >
+              <span className="text-center justify-content-center d-flex align-items-center w-75">
+                Log Out
+              </span>
+              <span className="w-25 justify-content-center d-flex align-items-center">
+                <IoIosLogOut className="size-20" />
+              </span>
+            </Button>
+          </Row>
+        </Container>
+      </Fade>
+      <Button
+        ref={personRef}
+        onClick={toggleVisibility}
+        className="m-auto bg-3 w-90 wide-button d-flex justify-content-center align-items-center p-1"
+      >
+        <span className="text-center justify-content-center d-flex align-items-center w-75">
+          {userName}
+        </span>
+        <span className="w-25 justify-content-center d-flex align-items-center">
+          <IoMdPerson className="size-20" />
+        </span>
+      </Button>
+
+      {showTimeline && <TimeLine />}
+    </Container>
   );
 }
 
--- project/myapp/frontend/src/components/SideMenuMiddle.js
+++ project/myapp/frontend/src/components/SideMenuMiddle.js
@@ -1,48 +1,77 @@
-import { Container, Button } from "react-bootstrap";
-import { useContext, useState } from "react";
-import TimeLine from "./TimeLine";
-import { TbTimelineEventFilled } from "react-icons/tb";
+import { Container, Button, Row } from "react-bootstrap";
+import { useContext, useState, useEffect } from "react";
 import PerfectScrollbar from "react-perfect-scrollbar";
 import { IoIosDocument } from "react-icons/io";
-import { Row } from "react-bootstrap";
 import { AuthContext } from "./AuthContextProvider";
 import { ResponseContext } from "./ResponseContextProvider";
+import axios from "axios";
+import { FaTrashAlt } from "react-icons/fa";
 
-function SideMenuMiddle() {
+function SideMenuMiddle({ clickedDocument, setClickedDocument }) {
+  const { user } = useContext(AuthContext);
+
   const [showTimeline, setShowTimeline] = useState(false);
   const { files } = useContext(AuthContext);
+  const { getFiles } = useContext(AuthContext);
   const {currentFile, setCurrentFile} = useContext(AuthContext);
   const {messages, setMessages} = useContext(ResponseContext);
-  const handleButtonClick = () => {
-    setShowTimeline(!showTimeline);
+
+  const baseURL = process.env.REACT_APP_API_URL;
+
+  
+
+  const getChatHistory = async (fileid) => {
+    const body = {
+      embedding_id: fileid ? fileid : currentFile
+    };
+    try {
+      const { data } = await axios.post(baseURL + "api/getchat/", body);
+      setMessages(data.messages);
+    } catch (error) {
+      console.error("Error fetching chat history:", error);
+      setMessages([]);
+    }
   };
+
+  const deleteDocument = async (fileid) => {
+    console.log('fileid:', fileid);
+    const resp = await axios.post(baseURL+'api/deletefile/', {fileid: fileid, user: user.id});
+
+    console.log(resp);
+    getFiles();
+  };
+
   const chooseDocument = (fileid) => {
-    console.log('file:', fileid)
     setCurrentFile(fileid);
-    setMessages([]);
+    localStorage.setItem('currentFile', fileid);
+    getChatHistory(fileid);
+    setClickedDocument(true); // Set clickedDocument to true when a document is chosen
   };
 
   return (
     <Container className="p-0 mt-3">
-      <Button
-          onClick={handleButtonClick}
-          className="m-auto bg-3 w-90 wide-button d-flex justify-content-center align-items-center p-1"
-        >
-          <span className="text-center justify-content-center d-flex align-items-center w-75">
-            Timeline
-          </span>
-          <span className="w-25 justify-content-center d-flex align-items-center">
-            <TbTimelineEventFilled className="size-20" />
-          </span>
-        </Button>
-      {showTimeline && <TimeLine />}
-      <PerfectScrollbar>
-      {files.map((file) => (
-        <Row className=" my-4 overflow-scroll m-auto rounded-2 w-100 bg-3">
-          <Button value={file.id} onClick={e => chooseDocument(e.target.value)} className="my-2 text-start"><IoIosDocument  size={30} /> { file.filename } </Button>
-        </Row>
-        ), [files])}
-        </PerfectScrollbar>
+      {clickedDocument ? (
+        <div>
+          asdasdasdasd
+        </div>
+      ) : (
+        <>
+          <hr className="w-90 m-auto" />
+          <PerfectScrollbar>
+            {files.map((file) => (
+              <Row key={file.id} className="my-4 m-auto br-5 w-100">
+                <Button
+                  onClick={() => chooseDocument(file.id)}
+                  value={file.id}
+                  className="m-auto bg-3 w-90 document-button d-flex justify-content-start align-items-center p-2 text-start"
+                >
+                  {file.filename}
+                </Button>
+              </Row>
+            ))}
+          </PerfectScrollbar>
+        </>
+      )}
     </Container>
   );
 }
