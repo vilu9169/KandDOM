c989e9f2e501ab8c1cc8a242d9a1c5757e5f578a
Rickrokyfy <01ahlkvist@gmail.com>
2024-04-24 12:46:52 +0200

    Björnkör

--- backend/tointegrate/summariser/usinggroq.py
+++ backend/tointegrate/summariser/usinggroq.py
@@ -64,13 +64,18 @@ def handlesplit(split, retvals, i):
         for tool_call in events.choices[0].message.tool_calls:
             args = json.loads(tool_call.function.arguments)
             #Add to dict
-            ret.append({"time": parser.parse(args["time"]), "information": args["information"]})
-            #dict[ parser.parse(args["time"])] = args["information"]
+            try :
+                ret.append({"time": parser.parse(args["time"]), "information": args["information"]})
+            except Exception as e:
+                print(e)
+                print("Could not parse time")
+                ret.append({"time": args["time"], "information": args["information"]})
         retvals[i] = ret
+        pass
     except Exception as e:
         print(e)
         print("No tool calls")
-        return
+        pass
         
 
 
@@ -91,7 +96,6 @@ splitsize = 10000
 splits = [text[i:i+splitsize] for i in range(0, len(text), splitsize)]
 ttime = time()
 # splits = [text]
-dict = []
 from dateutil import parser
 import threading
 
@@ -109,81 +113,26 @@ for i in range(len(splits)):
 # Wait for all threads to finish
 for thread in threads:
     thread.join()
-# for split in splits:
-    #create a thread for each split
-    
-    # # First summarise the events in the text
-    # while True:
-    #     try:
-    #         events = client.chat.completions.create(
-    #             messages=[{
-    #                 "role": "system",
-    #                 "content": "Summera händelserna i texten och inkludera när de skede. Du måste få med alla händelser. Svara på svenska. ",
-                    
-    #             },
-    #             {
-    #                 "role": "user",
-    #                 "content": split,
-    #             }],
-    #             model="llama3-70b-8192",
-    #             temperature=0.1,
-    #         )
-    #         break
-    #     except Exception as e:
-    #         print(e)
-    #         print("Error working with model")
-    #         #Sleep for 20 seconds
-    #         sleep(20)
-    #         continue
-    # split = str(events.choices[0].message.content)
-    # print("Newsplit = ", split)
-    # while True:
-    #     try:
-    #         events = client.chat.completions.create(
-    #             messages=[
-    #                 {
-    #                     "role": "system",
-    #                     "content": instructions,
-    #                 },
-    #                 {
-    #                     "role": "user",
-    #                     "content": split,
-    #                 }
-    #             ],
-    #             tools = [tools["timelinemaker"]],
-    #             model="llama3-70b-8192",
-    #             temperature=0.0,
-    #         )
-    #         break
-    #     except Exception as e:
-    #         print(e)
-    #         print("Error working with model")
-    #         #Sleep for 20 seconds
-    #         sleep(20)
-    #         continue
-    # print("Calling")
-    # print(str(events.choices[0].message))
-    # try:
-    #     for tool_call in events.choices[0].message.tool_calls:
-    #         args = json.loads(tool_call.function.arguments)
-    #         #Add to dict
-    #         dict.append({"time": parser.parse(args["time"]), "information": args["information"]})
-    #         #dict[ parser.parse(args["time"])] = args["information"]
-    # except Exception as e:
-    #     print(e)
-    #     print("No tool calls")
+
+#merge all the lists in retvals
+merge = []
+
+for ret in retvals:
+    merge += ret
+retvals = merge
+
 
 print("Time: ", time()-ttime)
-print(dict)
+print(retvals)
 def compfun(x):
     try:
         return x["time"].timestamp()
     except:
         return 0
 #Sort dict on keys, i.e. time. If parsing fails the time will be set to the epoch
-dict = sorted(dict, key = lambda x: compfun(x))
+retvals = sorted(retvals, key = lambda x: compfun(x))
 #Convert all the times to strings
-for event in dict:
+for event in retvals:
     event["time"] = event["time"].strftime("%Y-%m-%d %H:%M")
 
-print(dict)
+print(retvals)
