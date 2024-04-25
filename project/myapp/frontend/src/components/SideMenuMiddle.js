94b48933449067a8d2f7f03feb45d470a7e4d907
CarlJohan12 <carljohan.eklund.2@gmail.com>
2024-04-25 09:54:30 +0200

    Update SideMenuMiddle.js

--- project/myapp/frontend/src/components/SideMenuMiddle.js
+++ project/myapp/frontend/src/components/SideMenuMiddle.js
@@ -35,6 +35,11 @@ function SideMenuMiddle({ clickedDocument, setClickedDocument }) {
       {clickedDocument ? (
         pinnedMessages.map((pin) => (
           <Row key={pin.id} className="my-4 m-auto br-5 w-100">
+            <Button
+              classname="m-auto bg-3 w-90 document-button d-flex justify-content-start align-items-center p-2 text-start"
+              >
+                {pin.filename}
+              </Button>
             {pin}
           </Row>
         ))
