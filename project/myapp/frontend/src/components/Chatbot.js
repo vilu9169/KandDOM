851d2c91cc3c4d3a7e9c1d7320f343e7915f2964
vilu9169 <viktor.lundin3@gmail.com>
2024-04-25 15:22:16 +0200

    d

--- project/myapp/frontend/src/components/Chatbot.js
+++ project/myapp/frontend/src/components/Chatbot.js
@@ -68,7 +68,7 @@ const Chatbot = () => {
       <Container className="chatbot-messages w-100 p-0">
         {messages.map((message, index) => (
           <Container
-            ref={ref => pinRef.current[index] = ref}
+            ref={pinRef.current[index]}
             key={index}
             className={`message ${
               message.user ? "user-message" : "ai-message"
