8707a59804c01d99c0e153ec0e26ef352f1e7789
Björn Westerlund <bpf.westerlund@gmail.com>
2024-05-07 15:05:30 +0200

    added bedrock

---
+++ backend/tointegrate/relations_graph/bedrock_test.py
@@ -0,0 +1,45 @@
+import boto3
+import json
+import threading
+from time import time
+client = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
+
+def ask_question(client, model_id, prompt):
+    response = client.invoke_model(
+        modelId=model_id,
+        body=json.dumps(
+            {
+                "anthropic_version": "bedrock-2023-05-31",
+                "max_tokens": 10,
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": [{"type": "text", "text": prompt}],
+                    }
+                ],
+            }
+        ),
+    )
+
+    result = json.loads(response.get("body").read())
+    output_list = result.get("content", [])
+    for output in output_list:
+        print(output["text"])
+model_id = "anthropic.claude-3-haiku-20240307-v1:0"
+
+
+prompt = "what is the capital of france?"
+
+threads = [threading.Thread(target=ask_question, args=[client, model_id, prompt]) for _ in range(50)]
+
+start = time()
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+print("time elapsed:", time() - start)
+
+
+
