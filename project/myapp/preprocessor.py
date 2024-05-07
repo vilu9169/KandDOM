193527a1e29c4c9bacf87d4db0870580a784a17a
vilu9169 <viktor.lundin3@gmail.com>
2024-05-07 15:40:15 +0200

    new Rikard stuff

--- project/myapp/preprocessor.py
+++ project/myapp/preprocessor.py
@@ -1,3 +1,4 @@
+
 from pinecone import Pinecone, ServerlessSpec
 import PyPDF2
 # pdf_file = "gbg_mordforsok.pdf"
@@ -10,13 +11,20 @@ from pinecone import Pinecone, ServerlessSpec
 #from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_text_splitters import CharacterTextSplitter
 from . timelinemaker import analyzefromstr
+from time import sleep
 
 from google.cloud import documentai
 import io
 import threading
 
 def async_handle_chunk(chunk_num, chunk_size, num_pages, pdf_file, client, name, resstrings):
-    reader = PyPDF2.PdfReader(pdf_file)
+    while True:
+        try:
+            reader = PyPDF2.PdfReader(pdf_file)
+            break
+        except Exception as e:
+            print(e)
+            sleep(5)
     pagenr = chunk_num * chunk_size
     start_page = chunk_num * chunk_size
     end_page = min((chunk_num + 1) * chunk_size, num_pages)
@@ -45,7 +53,7 @@ def async_handle_chunk(chunk_num, chunk_size, num_pages, pdf_file, client, name,
     # Read the text recognition output from the processor
     resstring = ""
     for page in document.pages:
-        resstring += "{pagestart page "+ str(page_num+1) + " in document "+pdf_file +" }"
+        resstring += "{pagestart page "+ str(pagenr+1) + " in document "+pdf_file +" }"
         temp = ""
         for block in page.blocks:
             split = str(block.layout.text_anchor.text_segments).split("\n")
@@ -55,22 +63,102 @@ def async_handle_chunk(chunk_num, chunk_size, num_pages, pdf_file, client, name,
                 temp += document.text[0: int(split[0][split[0].find(":") +2:])]                
         resstring += temp
         #resstring += page.layout.text_anchor.content
-        resstring +="{pageend page "+ str(page_num+1)+ " in document "+pdf_file +"}"+str(chr(28))
-
+        resstring +="{pageend page "+ str(pagenr+1)+ " in document "+pdf_file +"}"+str(chr(28))
         pagenr += 1
     resstrings[chunk_num] = resstring
     pass
 
+def getraw(chunk_num, chunk_size, num_pages, pdf_file, num_chunks):
+    reader = PyPDF2.PdfReader(pdf_file)
+    start_page = chunk_num * chunk_size
+    end_page = min((chunk_num + 1) * chunk_size, num_pages)
+    docs = []
+    writer = PyPDF2.PdfWriter()
+    for page_num in range(start_page, end_page):
+        writer.add_page(reader.pages[page_num])
+    response_bytes_stream = io.BytesIO()
+    writer.write(response_bytes_stream)
+    #Seek 0 to start reading
+    response_bytes_stream.seek(0)
+    # Read the file into memory
+    with response_bytes_stream as image:
+        image_content = image.read()
+    # Load Binary Data into Document AI RawDocument Object
+    return image_content
+
+def swifthandle(chunk, resind, client, name, resstrings, chunksize):
+    raw_document = documentai.RawDocument(content=chunk, mime_type="application/pdf")
+    # Configure the process request
+    request = documentai.ProcessRequest(name=name, raw_document=raw_document)
+    result = client.process_document(request=request, )
+    #print("Result looks like ", result)
+    # For a full list of Document object attributes,
+    # please reference this page: https://googleapis.dev/python/documentai/latest/gapic/v1beta3/types.html#google.cloud.documentai.v1beta3.Document
+    document = result.document
+    # Read the text recognition output from the processor
+    pagenr = chunksize*resind
+    resstring = ""
+    for page in document.pages:
+        resstring += "{pagestart page "+ str(pagenr+1) + " in document "+pdf_file +" }"
+        temp = ""
+        for block in page.blocks:
+            split = str(block.layout.text_anchor.text_segments).split("\n")
+            if(len(split) > 2):
+                temp += document.text[int(split[0][split[0].find(":") +2:]) : int(split[1][split[1].find(":") +2:])]
+            else:
+                temp += document.text[0: int(split[0][split[0].find(":") +2:])]                
+        resstring += temp
+        #resstring += page.layout.text_anchor.content
+        resstring +="{pageend page "+ str(pagenr+1)+ " in document "+pdf_file +"}"+str(chr(28))
+
+        pagenr += 1
+    resstrings[resind] = resstring
+    pass
+
+#Same as before but no threads with pdf reader
+def ocr_pdf2(pdf_file, project_id, location, processor_id):
+    
+    # You must set the api_endpoint if you use a location other than 'us'.
+    opts = {"api_endpoint": "eu-documentai.googleapis.com"}
+
+    client = documentai.DocumentProcessorServiceClient(client_options=opts)
+    name = client.processor_path(project_id, location, processor_id)
+    # Create a working directory
+
+    # Split the document into chunks of 20 pages
+    chunk_size = 15
+    reader = PyPDF2.PdfReader(pdf_file)
+    num_pages = len(reader.pages)
+    if(num_pages%chunk_size == 0):
+        num_chunks = num_pages//chunk_size
+    else:
+        num_chunks = num_pages//chunk_size + 1
+        
+    threads = []
+    rawdata = []
+    # Get the raw data
+    #getraw 
+    for chunkid in range(num_chunks):
+        rawdata.append(getraw(chunkid, chunk_size, num_pages, pdf_file, num_chunks))
+    resstrings = []
+    for i in range(num_chunks):
+        resstrings.append("")
+        t = threading.Thread(target=swifthandle, args=(rawdata[i], i,  client, name, resstrings, chunk_size))
+        threads.append(t)
+        t.start()
+    # Wait for all threads to finish
+    for thread in threads:
+        thread.join()
+    resstring = ""
+    for res in resstrings:
+        resstring += res
+    return resstring
 def ocr_pdf(pdf_file, project_id, location, processor_id):
     
     # You must set the api_endpoint if you use a location other than 'us'.
     opts = {"api_endpoint": "eu-documentai.googleapis.com"}
 
     client = documentai.DocumentProcessorServiceClient(client_options=opts)
-
-    # The full resource name of the processor, e.g.:
-    # projects/project_id/locations/location/processor/processor_id
-    # You must create new processors in the Cloud Console first
     name = client.processor_path(project_id, location, processor_id)
     # Create a working directory
 
@@ -103,10 +191,6 @@ def ocr_pdf(pdf_file, project_id, location, processor_id):
     resstring = ""
     for res in resstrings:
         resstring += res
-    #Print to .txt file
-    with open("output.txt", 'w', encoding='utf-8') as file:
-        file.write(resstring)
-    #print("Resstring",resstring)
     return resstring
 
 def extract_text_from_pdf(pdf_file) -> str:
@@ -145,7 +229,7 @@ def text_to_rag(new_index_name, text):
     except Exception as e:
         print("Index already exists")
     # Split documents
-    text_splitter = CharacterTextSplitter(chunk_size=2, chunk_overlap =1,separator=str(chr(28)))
+    text_splitter = CharacterTextSplitter(chunk_size=2, chunk_overlap =0,separator=str(chr(28)))
     splits = [Document(page_content=x) for x in text_splitter.split_text(text)]
     #splits = text_splitter.split_text(text)    
     embeddings = VertexAIEmbeddings(model_name="textembedding-gecko-multilingual@001")
@@ -153,16 +237,31 @@ def text_to_rag(new_index_name, text):
     vectorstore = PineconeVectorStore(new_index_name, embeddings.embed_query, splits)
     # Vertex AI embedding model  uses 768 dimensions`
     vectorstore = vectorstore.from_documents(splits, embeddings, index_name=new_index_name)
-    
+
+def bettersort(theevents):
+    if(type(theevents["title"]) == str):
+        return 0
+    else:
+        return theevents["title"].timestamp()
+
 def handle_multi_pdfs(pdf_files, new_index_name):
     alltexts  = ""
+    retarr = []
     for pdf_file in pdf_files:
-        text = ocr_pdf(pdf_file, "sunlit-inn-417922", "eu", "54cf154d8c525451")
+        #text = ocr_pdf(pdf_file, "sunlit-inn-417922", "eu", "54cf154d8c525451")
+        text = ocr_pdf2(pdf_file, "sunlit-inn-417922", "eu", "54cf154d8c525451")
         text_to_rag(new_index_name, text)
         #After text to rag run timelinemaker
-        alltexts += text
-    res = analyzefromstr(alltexts)
-    print(res)
+        # alltexts += text
+        retarr += analyzefromstr(text, pdf_file)
+    retarr = sorted(retarr, key = lambda x: bettersort(x))
+    
+    for elem in retarr:
+        try:
+            elem["title"] = str(elem["title"].strftime("%d/%m/%Y %H:%M"))
+        except Exception as e:
+            print("Error parsing time: ", e)
+    print(retarr)
 #Takes a pdf file path and a new index name as input
 #Extracts text from the pdf file and converts it to RAG which is stored in the new index
 def mainfunk(pdf_file, new_index_name):
@@ -181,14 +280,14 @@ def mainfunk(pdf_file, new_index_name):
 # mainfunk(pdf_file, new_index_name)
 
 
-# pdf_files = []
-# while True:
-#     print("Name of the files to be processed using RAG(write \"end\" to end input): ")
-#     pdf_file = input()
-#     if(pdf_file == "end"):
-#         break
-#     else:
-#         pdf_files.append(pdf_file)
-# print("Name of the new index: ")
-# new_index_name = input()
-# handle_multi_pdfs(pdf_files, new_index_name)
+pdf_files = []
+while True:
+    print("Name of the files to be processed using RAG(write \"end\" to end input): ")
+    pdf_file = input()
+    if(pdf_file == "end"):
+        break
+    else:
+        pdf_files.append(pdf_file)
+print("Name of the new index: ")
+new_index_name = input()
+handle_multi_pdfs(pdf_files, new_index_name)
--- project/myapp/timelinemaker.py
+++ project/myapp/timelinemaker.py
@@ -7,7 +7,12 @@ from time import sleep
 import vertexai
 from pinecone import Pinecone
 from groq import Groq
+from anthropic import AnthropicVertex
 import json
+from vertexai.preview.generative_models import (
+    HarmCategory, 
+    HarmBlockThreshold )
+from google.cloud.aiplatform_v1beta1.types.content import SafetySetting
 tools = {
     "timelinemaker": {
         "type": "function",
@@ -19,22 +24,22 @@ tools = {
                 "properties": {
                     "time": {
                         "type": "string",
-                        "description": "Datum och tid då händelsen inträffade. Skall endast vara en tid per händelse. Tider måste alltid anges på formatet DD/MM/YY HH:MM.",
+                        "description": "Datum och tid då händelsen inträffade. Skall endast vara en tid per händelse. Tider kan vara på formatet år-månad-dag eller dag-månad år, du måste avgöra vilket utifrån kontexten. Tider måste alltid sparas på formatet DD/MM/YY HH:MM.",
                     },
                     "pages": {
+                        # "type": "array",
+                        # "items": {
+                        #     "type": "int"
+                        # },
                         "type": "string",
-                        "description": "Sidnummer till sidorna där information om händelsen finns.",
+                        "description": "Sidnummer till där information om händelsen finns. Varje sidnummer skall bara förekomma en gång.",
                     },
                     "information": {
                         "type": "string",
                         "description": "Information om händelsen.",
-                    },
-                    "document": {
-                        "type": "string",
-                        "description": "Namnet på dokumentet som händelsen hämtades ifrån. Namnet på dokumentet finns på samma plats som sidnumret.",
                     }
                 },
-                "required": ["time","pages","information", "document"],
+                "required": ["time","pages","information"],
             },
         },
     },
@@ -71,46 +76,88 @@ vectorstore = PineconeVectorStore(
     index, embeddings  
 )  
 
-def summarise_gemeni_par(input, index, res):
-    cont = "Du är en LLM som hämtar och dokumenterar händelser, när de skedde och på vilka sidor det finns information om dem. Alla dina svar måste vara på svenska. Dokumentera alla händelser du identifierar i texten med en beskrivning av händelsen och datum samt tidpunkten när den skede. Tidpunkter ska vara på formatet DD/MM/YY HH:MM. Var utförlig i händelsebeskrivningarna och tillse att de är på svenska. Du måste alltid inkludera vilka sidor du hittade informationen. Här är materialet du ska behandla  :" + input 
-    generation_config = {
-    "max_output_tokens": 4400,
-    "temperature": 0.1,
-    "top_p": 1,
-    }
-
-    safety_settings = {
-        generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    }
-    vertexai.init(project="sunlit-inn-417922", location="us-central1")
-    model = GenerativeModel("gemini-1.5-pro-preview-0409")
-    #model = GenerativeModel("gemini-1.0-pro")
-    
+#Summarise the content in some documents
+def summarise_claud(input, index, res):
+    global loc_haiku, optind_haiku, options_haiku, model_haiku
+    global loc_sonnet, optind_sonnet, options_sonnet, model_sonnet
+    # model = model_haiku
+    # loc = loc_haiku
+    # optind = optind_haiku
+    # options = options_haiku
+    model = model_sonnet
+    loc = loc_sonnet
+    optind = optind_sonnet
+    options = options_sonnet
+    # Set the endpoint URL
+    # context = "Skapa en tidslinje baserad på följande dokument. Använd bara information från detta dokument i dina svar och upprepa dig inte. Dethär är en sammanfattning av vad som skett "+ sammanf  
+    context = """Du är en LLM som hämtar och dokumenterar händelser, när de skedde och på vilka sidor det finns information om dem.
+    Dokumentera alla händelser du identifierar i texten med en beskrivning av händelsen och när den skede.
+    Tidpunkter ska vara på formatet DD/MM/YY HH:MM. Var utförlig i händelsebeskrivningarna. Sidnummer finns efter \"pagestart page\" och \"pageend page\" andra sidnummer ignoreras.
+    Namnet på dokumentet finns efter texten \"in document\", andra namn ignoreras. Här är materialet du ska behandla  :""" + input 
+    #Create a json struct for previous messages and the current message
+    client = AnthropicVertex(region=loc, project_id="sunlit-inn-417922")
     while True:
         try:
-            response = model.generate_content(
-                [cont],
-                generation_config=generation_config,
-                safety_settings=safety_settings,
+            print("loc: ", loc)
+            message = client.messages.create(
+            model=model,
+            messages = [{"content": ("Dokumentera alla händelser du identifierar i texten och inkludera tidpunkten när de sker. Alla relevanta händelser ska dokumenteras. Alla svar skall vara på svenska. Här är materialet du ska behandla  :" + input), "role": "user"}],
+            system = context,
+            max_tokens = 3000
             )
-            #print("Response: ", response)
-
-            try :
-                res[index]= response.text
-                return
-            except Exception as e:
-                print("Error likely due to block_reason for: ", e)
-                return
+            res[index] =  message.content[0].text
+            return
         except Exception as e:
             print("Error: ", e)
-            #Sleep for 20 seconds
-            sleep(20)
-     
+            optind+=1
+            if(optind==len(options)):
+                optind = 0
+            loc = options[optind]
+
+# def summarise_gemeni_par(input, index, res):
+#     cont = """Du är en LLM som hämtar och dokumenterar händelser, när de skedde och på vilka sidor det finns information om dem.
+#     Alla dina svar måste vara på svenska. Dokumentera alla händelser du identifierar i texten med en beskrivning av händelsen och datum samt tidpunkten när den skede.
+#     Tidpunkter ska vara på formatet DD/MM/YY HH:MM. Var utförlig i händelsebeskrivningarna och tillse att de är på svenska. Du måste alltid inkludera vilka sidor du hittade informationen, korrekt sidnummer finns efter \"pagestart page\" och \"pageend page\". Andra sidnummer ska ignoreras.
+#     Namnet på dokumentet finns efter texten \"in document\" och ska finnas med. Här är materialet du ska behandla  :""" + input 
+#     generation_config = {
+#     #"max_output_tokens": 4400,
+#     "temperature": 0, # 0.1,
+#     "top_p": 1,
+#     }
+
+#     safety_settings = {
+#         generative_models.HarmCategory.HARM_CATEGORY_HATE_SPEECH: generative_models.HarmBlockThreshold.BLOCK_NONE,
+#         generative_models.HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: generative_models.HarmBlockThreshold.BLOCK_NONE ,
+#         generative_models.HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: generative_models.HarmBlockThreshold.BLOCK_NONE ,
+#         generative_models.HarmCategory.HARM_CATEGORY_HARASSMENT: generative_models.HarmBlockThreshold.BLOCK_NONE ,
+#     }
+#     vertexai.init(project="sunlit-inn-417922", location="us-central1")
+#     model = GenerativeModel("gemini-1.5-pro-preview-0409")
+#     #model = GenerativeModel("gemini-1.0-pro")
+    
+#     while True:
+#         try:
+#             response = model.generate_content(
+#                 [cont],
+#                 generation_config=generation_config,
+#                 safety_settings=safety_settings,
+#             )
+#             #print("Response: ", response)
+
+#             try :
+#                 res[index]= response.text
+#                 return
+#             except Exception as e:
+#                 print("Response: ", response)
+#                 print("Error likely due to block_reason for: ", e)
+#                 return
+#         except Exception as e:
+#             print("Error: ", e)
+#             #Sleep for 20 seconds
+#             sleep(20)
+                 
 #Handles parsing a split using functions in LLAMA3 hosted on Groq
-def handlesplit(split, retvals, i):
+def handlesplit(split, retvals, i, docname):
     global groqind, groqalts
     gind = groqind
     galts = groqalts
@@ -141,7 +188,7 @@ def handlesplit(split, retvals, i):
                 ],
                 tools = [tools["timelinemaker"]],
                 model="llama3-70b-8192",
-                temperature=0.1,
+                #temperature=0.1,
             )
             break
         except Exception as e:
@@ -159,10 +206,10 @@ def handlesplit(split, retvals, i):
             args = json.loads(tool_call.function.arguments)
             #Add to dict
             try :
-                ret.append({"time": parser.parse(args["time"], dayfirst=True),"pages": args["pages"] , "information": args["information"],"document": args["document"]})
+                ret.append({"title": parser.parse(args["time"], dayfirst=True),"pages": args["pages"] , "cardTitle": args["information"],"document": docname})
             except Exception as e:
                 #Append time anyways
-                ret.append({"time": args["time"],"pages": args["pages"] , "information": args["information"], "document": args["document"]})
+                ret.append({"title": args["time"],"pages": args["pages"] , "cardTitle": args["information"], "document": docname})
         retvals[i] = ret
         pass
     except Exception as e:
@@ -171,19 +218,22 @@ def handlesplit(split, retvals, i):
         pass
     
 def bettersort(theevents):
-    if(type(theevents["time"]) == str):
+    if(type(theevents["title"]) == str):
         return 0
     else:
-        return theevents["time"].timestamp()
-def analyzefromstr(input):
+        return theevents["title"].timestamp()
+
+def analyzefromstr(input, docname):
     from langchain.docstore.document import Document
 
     doc =  Document(page_content=input, metadata={"source": "local"})
     #Load inputstring as a document
     # Split documents
-    maxclaudin = 50000
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = maxclaudin, chunk_overlap = 0)
+    maxclaudin = 10000
+    #maxclaudin = 2000
+    from langchain.text_splitter import CharacterTextSplitter
+    text_splitter = CharacterTextSplitter(chunk_size = maxclaudin, chunk_overlap = 0, separator=str(chr(28)))
+    # text_splitter = RecursiveCharacterTextSplitter(chunk_size = maxclaudin, chunk_overlap = 0)
     splits = text_splitter.split_documents([doc])
     print("number of splits: ", len(splits))
     timelines = []
@@ -195,7 +245,8 @@ def analyzefromstr(input):
     print("Timeline length: ", len(timelines))
     #Summarise parts of the text
     for elem in splits:
-        t = threading.Thread(target=summarise_gemeni_par, args=(elem.page_content, index, timelines))
+        #t = threading.Thread(target=summarise_gemeni_par, args=(elem.page_content, index, timelines))
+        t = threading.Thread(target=summarise_claud, args=(elem.page_content, index, timelines))
         index += 1
         threads.append(t)
         t.start()
@@ -208,7 +259,7 @@ def analyzefromstr(input):
     retvals = []
     for i in range(len(timelines)):
         retvals.append([])
-        t = threading.Thread(target=handlesplit, args=(timelines[i], retvals, i))
+        t = threading.Thread(target=handlesplit, args=(timelines[i], retvals, i, docname))
         threads.append(t)
         t.start()
 
@@ -220,11 +271,11 @@ def analyzefromstr(input):
         struct += elem
 
     struct = sorted(struct, key = lambda x: bettersort(x))
-    for elem in struct:
-        try:
-            elem["time"] = str(elem["time"].strftime("%d/%m/%Y %H:%M"))
-        except Exception as e:
-            print("Error parsing time: ", e)
+    # for elem in struct:
+    #     try:
+    #         elem["title"] = str(elem["title"].strftime("%d/%m/%Y %H:%M"))
+    #     except Exception as e:
+    #         print("Error parsing time: ", e)
     return struct
 
 # timerstart = time()
