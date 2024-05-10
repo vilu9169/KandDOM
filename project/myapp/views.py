622ad1ef1da207f684728fb942cc49b717e2c035
EC2 Default User <ec2-user@ip-172-31-9-254.eu-north-1.compute.internal>
2024-05-10 14:11:55 +0000

    huh

--- project/myapp/views.py
+++ project/myapp/views.py
@@ -307,8 +307,8 @@ embeddings = VertexAIEmbeddings(model_name="textembedding-gecko-multilingual@001
 @api_view(['POST'])
 def upload_document(request):
     file_obj = request.FILES.get('file')
-    group = request.data.get('group')
-    print(not group)
+    group = request.data.get('group') == 'true'
+    print(request.data)
     print(request.data['userID'])
     
     """print("All documents in the database:")
